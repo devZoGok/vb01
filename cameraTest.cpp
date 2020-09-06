9c18dc0bf52ff0e73a36b5f8ace5e8ff1bda81e2
devZoGok <nenkokeko@yandex.com>
2020-09-06 22:32:30 +0300

    Kameros testas
    Konfliktų sprendimai

---
+++ cameraTest.cpp
@@ -0,0 +1,26 @@
+#include"cameraTest.h"
+#include<cppunit/TestCase.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestCaller.h>
+
+using namespace CppUnit;
+
+namespace vb01{
+
+	CameraTest::CameraTest(){
+	}
+
+	void CameraTest::setUp(){
+		cam = new Camera();
+	}
+
+	void CameraTest::tearDown(){
+		delete cam;
+	}
+
+	void CameraTest::testLookAt(){
+		cam->lookAt(Vector3(0, 0, -1));
+		CPPUNIT_ASSERT(cam->getLeft() == Vector3(1, 0, 0));
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include "cameraTest.h"
+
+using namespace CppUnit;
+using namespace vb01;
+
 int main(){
+	TextUi::TestRunner runner;
+	runner.addTest(CameraTest::suite());
+	runner.run();
 	return 0;
 }
