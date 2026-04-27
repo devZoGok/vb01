83ef1d79636d6eb79102b75c949c81dbcd7ba2ee
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2026-04-27 20:08:29 +0200

    refactor: restructure monorepo

---
+++ Source/GUI/Private/listbox.cpp
@@ -0,0 +1,234 @@
+#include <GLFW/glfw3.h>
+
+#include "listbox.h"
+#include "util.h"
+#include "node.h"
+#include "text.h"
+#include "quad.h"
+#include "root.h"
+#include "material.h"
+
+using namespace vb01;
+using namespace std;
+
+namespace vb01Gui{
+	Listbox::ListboxButton::ListboxButton(Listbox *l, Vector3 pos, Vector2 size, string name) : Button(pos, size, name, "", -1, false), listbox(l) {}
+
+	void Listbox::ListboxButton::onClick(){
+		if(!listbox->isOpen())
+			listbox->openUp();
+		else{
+				Vector2 cursorPos = getCursorPos();
+
+				if(cursorPos.x < listbox->getScrollingButton()->getPos().x){
+					if(listbox->isCloseable())
+						listbox->close();
+				}
+				else
+					listbox->scrollToHeight(cursorPos.y);
+		}
+	}
+
+	Listbox::ScrollingButton::ScrollingButton(Vector3 pos, Vector2 size, string name) : Button(pos, size, name, "", -1, false){}
+
+	void Listbox::ScrollingButton::onClick(){}
+
+	Listbox::Listbox(Vector3 p, Vector2 s, std::vector<string> &lines, int md, string fp, bool cl) : 
+		pos(p),
+		size(s),
+		maxDisplay(md),
+		fontPath(fp),
+		closeable(cl),
+		textOffset(Vector3(0, 0, .3))
+	{
+		Root *root = Root::getSingleton();
+		Vector2 mousePos = getCursorPos();
+		guiNode = Root::getSingleton()->getGuiNode();
+
+		string texUni = "texturingEnabled", diffColUni = "diffuseColor";
+		textMat = new Material(root->getLibPath() + "text");
+		textMat->addBoolUniform(texUni, false);
+		textMat->addVec4Uniform(diffColUni, Vector4::VEC_IJKL);
+
+		if(!lines.empty()){
+			for(int i = 0; i < lines.size(); i++)
+				addLine(stringToWstring(lines[i]));
+
+			this->lines[0]->getNode()->setVisible(true);
+		}
+
+		listboxButton = new ListboxButton(this, pos, size, "ListboxButton");
+
+		scrollingButton = new ScrollingButton(Vector3(pos.x + size.x - 20, pos.y + lineHeight, pos.z - .05), Vector2(20, lineHeight * (maxDisplay - 2) / (maxDisplay + 1)), "scrollingButton");
+		scrollingButton->setColor(Vector4(.2, .2, .2, 1));
+		scrollingButton->setActive(false);
+
+		Quad *selRect = new Quad(Vector3(size.x, size.y, 0), false);
+		selRectNode = new Node(Vector3(size.x, size.y, -.05));
+		Material *mat = new Material(root->getLibPath() + "gui");
+		mat->addBoolUniform(texUni, false);
+		mat->addVec4Uniform(diffColUni, Vector4(.6, .35, .05, 1));
+		selRect->setMaterial(mat);
+		selRectNode->attachMesh(selRect);
+		selRectNode->setVisible(false);
+		guiNode->attachChild(selRectNode);
+
+		if(!closeable)
+			openUp();
+	}
+
+	Listbox::~Listbox(){
+		while(!lines.empty()){
+			int id = lines.size() - 1;
+			Node *textNode = lines[id]->getNode();
+			guiNode->dettachChild(textNode);
+			delete textNode;
+			lines.pop_back();
+		}
+
+		guiNode->dettachChild(selRectNode);
+		delete selRectNode;
+	}
+
+	//TODO selection rectangle Z offset
+	void Listbox::update(){
+		scrollingButton->update();
+		listboxButton->update();
+
+		if(open){
+			Vector2 mousePos = getCursorPos();
+			Vector3 p = pos;
+
+			if(mousePos.y > pos.y && mousePos.y < pos.y + size.y * maxDisplay){
+				selectedOption = scrollOffset + ((int)(mousePos.y - pos.y) / ((int)size.y));
+				p.y = pos.y + size.y * ((int)(mousePos.y - pos.y) / ((int)size.y));
+			}
+			else if(mousePos.y < pos.y){
+				selectedOption = 0;
+				p.y = pos.y;
+			}
+			else{
+				selectedOption = (maxDisplay + scrollOffset) - 1;
+				p.y = pos.y + size.y * (maxDisplay - 1);
+			}
+
+			selRectNode->setPosition(Vector3(p.x, p.y, p.z + .05));
+
+			Vector3 scrollButtonPos = Vector3(pos.x + size.x - 20, pos.y + lineHeight + lineHeight * scrollOffset * (double(maxDisplay - 2) / (maxDisplay + 1)), pos.z - .05);
+			scrollingButton->setPos(scrollButtonPos);
+		} 
+	}
+
+	void Listbox::scrollToHeight(float clickPos){
+		float clickHeight = clickPos - (pos.y + lineHeight);
+		int newOffset = clickHeight / (lineHeight * (maxDisplay - 2) / (maxDisplay + 1));
+		int diffOffset = scrollOffset - newOffset;
+
+		for(int i = 0; i < abs(diffOffset); ++i){
+				if(diffOffset > 0)
+					scrollUp();
+				else
+					scrollDown();
+		}
+	}
+
+	void Listbox::openUp(){
+		if(lines.empty()) return;
+
+		open = true;
+		Vector3 selOpPos = pos + Vector3(0, size.y * (selectedOption - scrollOffset + 1), 0) + textOffset;
+		lines[selectedOption]->getNode()->setPosition(selOpPos);
+		lines[selectedOption]->getNode()->setVisible(false);
+		Vector2 size = listboxButton->getSize();
+		size.y *= maxDisplay;
+		listboxButton->setSize(size);
+		selRectNode->setVisible(true);
+		scrollingButton->setActive(true);
+
+		for(int i = scrollOffset; i < scrollOffset + maxDisplay; i++)
+			lines[i]->getNode()->setVisible(true);
+
+		onOpen();
+	}
+
+	void Listbox::close(){
+		open = false;
+		Vector2 size = listboxButton->getSize();
+		size.y /= maxDisplay;
+		listboxButton->setSize(size);
+		selRectNode->setVisible(false);
+		scrollingButton->setActive(false);
+
+		for(int i = scrollOffset; i < scrollOffset + maxDisplay; i++)
+			lines[i]->getNode()->setVisible(false);
+
+		lines[selectedOption]->getNode()->setPosition(pos + Vector3(0, size.y, 0) + textOffset);
+		lines[selectedOption]->getNode()->setVisible(true);
+
+		onClose();
+	}
+
+	void Listbox::scrollUp(){
+		if(scrollOffset > 0){
+			scrollOffset--;
+
+			for(Text *l : lines){
+				Node *node = l->getNode();
+				Vector3 p = node->getPosition();
+				node->setPosition(Vector3(p.x, p.y + size.y, p.z));
+
+				if(node->getPosition().y > pos.y + size.y * maxDisplay)
+					node->setVisible(false);
+				else if(node->getPosition().y > pos.y)
+					node->setVisible(true);
+			}
+		}
+	}
+
+	void Listbox::scrollDown(){
+		if (scrollOffset < lines.size() - maxDisplay){
+			scrollOffset++;
+
+			for(Text *l : lines){
+				Node *node = l->getNode();
+				Vector3 p = node->getPosition();
+				node->setPosition(Vector3(p.x, p.y - size.y, p.z));
+
+				if(node->getPosition().y - size.y < pos.y)
+					node->setVisible(false);
+				else if(node->getPosition().y < pos.y + size.y * (maxDisplay + 1))
+					node->setVisible(true);
+			}
+		}
+	}
+
+	void Listbox::changeLine(int id, wstring change){
+		lines[id]->setText(change);	
+	}
+
+	std::vector<wstring> Listbox::getContents(){
+		std::vector<wstring> lines;
+
+		for(Text *t : this->lines)
+			lines.push_back(t->getText());
+
+		return lines;
+	}
+
+	void Listbox::addLine(wstring line){
+		float sc = .2;
+		Node *node = new Node(pos + Vector3(0, size.y * (lines.size() + 1), 0) + textOffset, Quaternion::QUAT_W, Vector3(sc, sc, 1));
+		Root::getSingleton()->getGuiNode()->attachChild(node);
+		node->setVisible(false);
+
+		Text *text = new Text(fontPath, line);
+		text->setMaterial(textMat);
+		node->addText(text);
+		
+		lines.push_back(text);
+	}
+
+	string Listbox::convert(string str){
+		return str;
+	}
+}
---
+++ Source/Rendering/Private/particleEmitter.cpp
@@ -0,0 +1,259 @@
+#include <algorithm>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <cstdlib>
+#include <ext.hpp>
+
+#include "particleEmitter.h"
+#include "shader.h"
+#include "material.h"
+#include "node.h"
+#include "root.h"
+#include "camera.h"
+
+using namespace glm;
+using namespace std;
+
+namespace vb01{
+	ParticleEmitter::ParticleEmitter(int numParticles) : Animatable(Animatable::PARTICLE_EMITTER){
+		this->numParticles = numParticles;
+
+		Vertex v1, v2, v3, v4, v5, v6;
+		ParticleEmitter::Vertex vertices[] = {v1, v2, v3, v4, v5, v6};
+		u32 indices[] = {0, 1, 2, 3, 4, 5};
+
+		setupParticles(vertices);
+		setupDisplay(vertices, indices);
+	}
+
+	void ParticleEmitter::setupParticles(Vertex vertices[]){
+		particles = new Particle*[numParticles];
+
+		Vector2 size = Vector2(.5,.5);
+		vertices[0].pos = Vector3(size.x / 2, size.y / 2, 0);
+		vertices[0].texCoords = Vector2(1, 1);
+
+		vertices[1].pos = Vector3(-size.x / 2, size.y / 2, 0);
+		vertices[1].texCoords = Vector2(0, 1);
+
+		vertices[2].pos = Vector3(-size.x / 2,-size.y / 2, 0);
+		vertices[2].texCoords=Vector2(0,0);
+
+		vertices[3].pos = Vector3(-size.x / 2, -size.y / 2, 0);
+		vertices[3].texCoords = Vector2(0, 0);
+
+		vertices[4].pos = Vector3(size.x / 2, -size.y / 2, 0);
+		vertices[4].texCoords = Vector2(1, 0);
+
+		vertices[5].pos = Vector3(size.x / 2, size.y / 2, 0);
+		vertices[5].texCoords = Vector2(1, 1);
+		
+		matrices = new mat4[numParticles];
+
+		for(int i = 0; i < numParticles; i++){
+			Particle *p = new Particle;
+			particles[i] = p;
+			matrices[i] = mat4(1.f);
+		}
+
+	}
+
+	void ParticleEmitter::setupDisplay(Vertex vertices[], u32 indices[]){
+		u32 MBO;
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(Vertex), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(u32), indices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, texCoords)));
+		glEnableVertexAttribArray(1);
+
+		glGenBuffers(1, &MBO);
+		glBindBuffer(GL_ARRAY_BUFFER, MBO);
+		glBufferData(GL_ARRAY_BUFFER, numParticles * sizeof(mat4), &matrices[0], GL_STATIC_DRAW);
+		glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(mat4), (void*)0);
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(mat4), (void*)(1 * sizeof(vec4)));
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(mat4), (void*)(2 * sizeof(vec4)));
+		glEnableVertexAttribArray(4);
+		glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(mat4), (void*)(3 * sizeof(vec4)));
+		glEnableVertexAttribArray(5);
+
+		glVertexAttribDivisor(2, 1);
+		glVertexAttribDivisor(3, 1);
+		glVertexAttribDivisor(4, 1);
+		glVertexAttribDivisor(5, 1);
+	}
+
+	ParticleEmitter::~ParticleEmitter(){
+		glDeleteVertexArrays(1, &VAO);	
+		glDeleteBuffers(1, &VBO);	
+
+		delete material;
+
+		delete[] particles;
+	}
+
+	void ParticleEmitter::makeHeap(int offset){
+		bool heap = false;
+
+		while(!heap){
+			bool end = true;
+
+			for(int i = 0; 2 * i + 1 + offset < numParticles; i++){
+				if(2 * i + 2 + offset < numParticles && (particles[i + offset]->distToCamPlane < particles[2 * i + 1 + offset]->distToCamPlane ||
+						   	particles[i + offset]->distToCamPlane < particles[2 * i + 2 + offset]->distToCamPlane))
+				{
+					bool leftChild = particles[2 * i + 1 + offset]->distToCamPlane > particles[2 * i + 2 + offset]->distToCamPlane;
+					swap(particles[i + offset], leftChild ? particles[2 * i + 1 + offset] : particles[2 * i + 2 + offset]);
+				}
+				else if(2 * i + 2 + offset == numParticles && particles[i + offset]->distToCamPlane < particles[2 * i + 1 + offset]->distToCamPlane)
+					swap(particles[i + offset], particles[2 * i + 1 + offset]);
+			}
+
+			for(int i = 0; 2 * i + 1 < numParticles; i++){
+				if(2 * i + 2 + offset < numParticles && (particles[i + offset]->distToCamPlane < particles[2 * i + 1 + offset]->distToCamPlane ||
+						   	particles[i + offset]->distToCamPlane < particles[2 * i + 2 + offset]->distToCamPlane))
+				{
+					end = false;
+				}
+				else if(2 * i + 2 + offset == numParticles && particles[i + offset]->distToCamPlane < particles[2 * i + 1 + offset]->distToCamPlane)
+					end = false;
+			}
+
+			if(end)
+				heap = true;
+		}
+	}
+	
+	void ParticleEmitter::heapSort(){
+		for(int i = 0; i < numParticles; i++)
+			makeHeap(i);
+	}
+	
+	void ParticleEmitter::update(){
+		Root *root = Root::getSingleton();
+		Camera *cam = root->getCamera();
+		float fov = cam->getFov(), width = root->getWidth(), height = root->getHeight();
+		float nearPlane = cam->getNearPlane(), farPlane = cam->getFarPlane();
+		Vector3 pos = node->getPosition();
+		Vector3 camDir = cam->getDirection(), up = cam->getUp(), left = cam->getLeft(), camPos = cam->getPosition();
+
+		mat4 view = lookAt(vec3(camPos.x, camPos.y, camPos.z), vec3(camPos.x + camDir.x, camPos.y + camDir.y, camPos.z + camDir.z), vec3(up.x, up.y, up.z));
+		mat4 proj = perspective(radians(fov), width / height, nearPlane, farPlane);
+
+		material->update();	
+
+		Shader *shader = material->getShader();
+		const string line = "const int numParticles = " + to_string(numParticles) + ";";
+		shader->editShader(Shader::VERTEX_SHADER, 10, line);
+		shader->editShader(Shader::FRAGMENT_SHADER, 6, line);
+
+		updateParticles(camDir, camPos);
+
+		shader->setMat4(view, "view");
+		shader->setMat4(proj, "proj");
+		shader->setVec4(startColor, "startColor");
+		shader->setVec4(endColor, "endColor");
+		shader->setVec2(size, "size");
+
+		heapSort();
+		render();
+	}
+
+	void ParticleEmitter::updateParticles(Vector3 camDir, Vector3 camPos){
+		Shader *shader = material->getShader();
+		Vector3 nodePos = node->localToGlobalPosition(Vector3::VEC_ZERO);
+		Vector3 nodeDir = node->getGlobalAxis(2);
+		Vector3 nodeUp = node->getGlobalAxis(1);
+		Vector3 nodeLeft = node->getGlobalAxis(0);
+
+		for(int i = 0; i < numParticles; i++){
+			if(getTime() - particles[i]->time >= particles[i]->timeToLive){
+				Vector3 dir = nodeDir;
+				float factor = (float)(rand() % 100) / 100;
+				particles[i]->time = getTime();
+				particles[i]->timeToLive = s64(1000 * ((highLife - lowLife) * factor + lowLife));
+
+				float spr1 = float(rand() % (int)spread) / 180 * PI, spr2 = float(rand() % 360) / 180 * PI;
+				Vector3 ra1 = (nodeUp.cross(dir)).norm(), ra2 = dir.norm();
+
+				if(ra1 == Vector3::VEC_ZERO)
+				   	ra1 = Vector3::VEC_I;
+
+				dir = Quaternion(spr1, ra1) * dir;
+				dir = Quaternion(spr2, ra2) * dir;
+
+				particles[i]->dir = dir.norm() * speed + gravity;
+				particles[i]->trans = nodePos;
+			}
+
+			particles[i]->trans = particles[i]->trans + particles[i]->dir;
+			particles[i]->distToCamPlane = cos(camDir.getAngleBetween((particles[i]->trans - camPos).norm())) * camPos.getDistanceFrom(particles[i]->trans);
+
+			float lifePercentage = (float)(getTime() - particles[i]->time) / particles[i]->timeToLive;
+
+			shader->setFloat(lifePercentage, "lifePercentage[" + to_string(i) + "]");
+			shader->setVec3(particles[i]->trans, "trans[" + to_string(i) + "]");
+		}
+	}
+
+	void ParticleEmitter::render(){
+		glBindVertexArray(VAO);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0, numParticles);	
+	}
+
+	void ParticleEmitter::animate(float value, KeyframeChannel keyframeChannel){
+		switch(keyframeChannel.type){
+			case KeyframeChannel::PARTICLE_EMITTER_SPEED:
+				speed = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_SPREAD:
+				spread = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_START_COLOR_W:
+				startColor.w = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_START_COLOR_X:
+				startColor.x = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_START_COLOR_Y:
+				startColor.y = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_START_COLOR_Z:
+				startColor.z = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_END_COLOR_W:
+				endColor.w = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_END_COLOR_X:
+				endColor.x = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_END_COLOR_Y:
+				endColor.y = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_END_COLOR_Z:
+				endColor.z = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_SIZE_X:
+				size.x = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_SIZE_Y:
+				size.y = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_LOW_LIFE:
+				lowLife = value;
+				break;
+			case KeyframeChannel::PARTICLE_EMITTER_HIGH_LIFE:
+				highLife = value;
+				break;
+		}
+	}
+}
